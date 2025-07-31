72852a541d93cd260c8a3a49e9e13b43895957eb
unknown <sushichaudhary51@gmail.com>
2025-07-31 17:07:44 +0530

    fffffff

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="container text-center my-5">
+      <h1 className="display-4">404</h1>
+      <p className="lead">Page Not Found</p>
+      <a href="/" className="btn btn-primary mt-3">Go to Home</a>
+    </div>
+  );
+};
+
+export default NotFound;
